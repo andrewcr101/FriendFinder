25524d0231f3be6648be52d7dbc2fb60da03e3a3
andrewcr101 <andrew.crawford@ge.com>
2018-10-15 10:10:31 -0400

    version z

---
+++ app/data/friends.js
@@ -0,0 +1,114 @@
+var friendsArray = [
+	{
+		name: 'UGA',
+		photo: 'https://www.mascotcosplay.com/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/u/g/uga_mascots_bulldog-1.jpg',
+		scores: [
+			'5',
+			'1',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Big Al',
+		photo: 'https://cdn.shopify.com/s/files/1/0160/6394/products/USB9175.jpeg?v=1502481427',
+		scores: [
+			'1',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Albert',
+		photo: 'https://i.pinimg.com/originals/0b/2d/a8/0b2da8e49aab104432d30890a42ccc15.jpg',
+		scores: [
+			'3',
+			'4',
+			'1',
+			'1',
+			'2'
+		]
+	},
+	{
+		name: 'Aubie',
+		photo: 'http://api.ning.com/files/LzTY94yzCcKYNSge5rvOjDvV-nupwP*vLVbAYix5VDXV-hE0Bz9NiwlaRTpk04Fk0Qx6tCqy1ACGBvlsSbC*QcVSVc1E1htm/auburntigers.com.jpg',
+		scores: [
+			'3',
+			'5',
+			'4',
+			'2',
+			'1'
+		]
+	},
+	{
+		name: 'Smokie',
+		photo: 'https://i.pinimg.com/originals/e8/fa/d8/e8fad8a6c2bdffc38d552b585c738d1f.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Clio',
+		photo: 'http://www.theclio.com/web/ul/50259.129746.jpg',
+		scores: [
+			'2',
+			'2',
+			'2',
+			'2',
+			'2'
+		]
+	},
+	{
+		name: 'Truman',
+		photo: 'https://imagesvc.timeincapp.com/v3/fan/image?url=https%3A%2F%2Fallfortennessee.com%2Ffiles%2F2013%2F10%2F7624786.jpg&c=sc&w=850&h=560',
+		scores: [
+			'3',
+			'3',
+			'3',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Caddie',
+		photo: 'https://www.kyforward.com/wp-content/uploads/2016/10/kentucky-mascot.jpg',
+		scores: [
+			'1',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Bully',
+		photo: 'https://i.pinimg.com/originals/e8/d3/03/e8d30317b9c40306b76b40def0587ba0.jpg',
+		scores: [
+			'4',
+			'4',
+			'1',
+			'4',
+			'4'
+		]
+	},
+	{
+		name: 'Madison',
+		photo: 'https://i.iheart.com/v3/re/new_assets/5b7177976f68b199ac7fe3f8',
+		scores: [
+			'4',
+			'4',
+			'4',
+			'4',
+			'4'
+		]
+	}
+];
+
+module.exports = friendsArray;
